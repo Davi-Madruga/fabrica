603b43c792424dfbb3b661d31eb2779840c2d996
F3IT3 <davimadruga@outlook.com.br>
2025-09-11 21:56:24 -0300

    requirements.txt

--- appcbv/urls.py
+++ appcbv/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HelloView, CachorroListView
 
 urlpatterns = [
-    path("",HelloView.as_view(),name='index'),
-    path("listar/",CachorroListView.as_view(),name='listar_cachorros'),
+    path("hello/",HelloView.as_view(),name='index'),
+    path("",CachorroListView.as_view(),name='listar_cachorros'),
 ]
